916f872d5e3151028c9ac561a2b0626d611fec8a
Mohammad Sina <mohammadsinazarei1102@gmail.com>
2026-06-25 16:06:26 +0330

    In this commit,I was working on sessionsclear

--- sina.py
+++ sina.py
@@ -1,20 +1,40 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template, session, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = "sinasina789"
 
-@app.route("/",methods=["GET" , "POST"])
-def start():
+
+@app.route("/", methods=["GET", "POST"])
+def main_index():
+
     if request.method == "POST":
+
         username = request.form.get("username")
-        password = request.form.get("password")
-        return f"Username is {username}"
-    
+        user_id = request.form.get("id")
+
+        session["username"] = username
+        session["user_id"] = user_id
+
+        print("SESSION AFTER SAVING:", dict(session))
+
+        return redirect(url_for("show_situation"))
+
     return render_template("form.html")
 
-@app.route("/result-route" , methods=["POST" , "GET"])
-def show():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    return render_template("result.html",username=username,password=password)
-    
 
+@app.route("/situation")
+def show_situation():
+
+    print("SESSION IN SITUATION:", dict(session))
+
+    username_s = session.get("username")
+    id_s = session.get("user_id")
+
+    if username_s is None or id_s is None:
+        return "Please fill out the form!"
+
+    return f"""
+    <h1>Information</h1>
+    <p>Username: {username_s}</p>
+    <p>ID: {id_s}</p>
+    """
