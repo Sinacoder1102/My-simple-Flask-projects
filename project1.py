981b58ea663215f7443a1db6f4df1bfec3465061
Mohammad Sina <mohammadsinazarei1102@gmail.com>
2026-06-27 16:18:35 +0330

    In this commit,I added logout mode to my project

--- project1.py
+++ project1.py
@@ -1,30 +1,37 @@
-from flask import Flask, redirect,request,render_template,session,make_response
+from flask import Flask,request,render_template,session,redirect
 
 app = Flask(__name__)
-app.secret_key = "mysecret9865"
+app.secret_key = "Sina1234"
 
 @app.route("/")
 def index():
-    return render_template("form12.html")
+    return render_template("form.html")
 
 @app.route("/login",methods=["POST"])
 def login():
+    return render_template("loginform.html")
+
+@app.route("/result", methods=["GET", "POST"])
+def show_the_final_result():
+
     if request.method == "POST":
         username = request.form.get("username")
-        user_id = request.form.get("userid")
-        password = request.form.get("password")
+        userid = request.form.get("userid")
 
-        if username == "admin" and password == "4567":
-            session["username"] = username
-            session["user_id"] = user_id
-            return redirect("/result")
-        else:
-            return "<h1>wrong username or password for admin</h1>"
-    if request.method == "GET":
+        session["username"] = username
+        session["userid"] = userid
+
+    if "username" not in session or session["username"] is None:
         return redirect("/")
 
-@app.route("/result",methods=["POST"])
-def check():
-    if "username" not in session:
-        return redirect("/")
-    return render_template("result.html",username1 = session.get("username"),user_id1 = session.get("user_id"))
+    return render_template(
+        "result.html",
+        username=session.get("username"),
+        userid=session.get("userid")
+    )
+
+
+@app.route("/logout",methods=["POST"])
+def logout():
+    session.clear()
+    return redirect("/")
