c0c3eca87eca69528d3b6736b34b55ff0aa04a66
Mohammad Sina <mohammadsinazarei1102@gmail.com>
2026-07-02 00:07:16 +0330

    I think this code has some mistakes and I'll fix it tommorrow......

--- sina.py
+++ sina.py
@@ -1,30 +1,83 @@
 # Importing models
-from flask import Flask,request,session,render_template
+import datetime
+import psycopg as postgres
+from flask import Flask,request,session,render_template,redirect,url_for
 from datetime import timedelta
 
-# The main app
+# Settings
 app = Flask(__name__)
-app.secret_key = "Server's_reload245"
-app.permanent_session_lifetime = timedelta(seconds=10)
+app.secret_key = "Helloworld544313"
+app.permanent_session_lifetime = datetime.timedelta(seconds=20)
 
+# Getting connection
+def get_connection():
+    return postgres.connect(
+        host = "localhost",
+        dbname = "postgres",
+        user = "postgres",
+        password = "21200",
+        port = 5432
+    )
+
+# Login Route
+@app.route("/login",methods=["GET","POST"])
+def login():
+    return render_template("loginform.html")
+
+# The main route
 @app.route("/")
-def loginpage():
-    return render_template("loginform01.html")
+def index():
+    return redirect(url_for("login")) 
 
-@app.route("/saving",methods=["POST"])
-def show_it():
+# The result route
+@app.route("/result",methods=["POST"])
+def show_the_final_result():
     username = request.form.get("username")
     password = request.form.get("password")
     user_id = request.form.get("userid")
 
-    session.permanent = True
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        """INSERT INTO users(username,password,user_id)""",
+        (username,user_id,password)
+    )
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
 
     session["username"] = username
     session["userid"] = user_id
 
-    return render_template("deletelog.html" , message = "Your informations was saved succesfully!")
+    session.permanent = True
 
+    get_username = session.get("username")
+    get_userid = session.get("userid")
+
+    return redirect(url_for("dashboard"))
+
+@app.route("/dashboard")
+def dashboard():
+
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM users")
+
+    users = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return str(users)
+
+# The logout route
 @app.route("/logout",methods=["POST"])
-def log_out_user():
+def deleteing():
     session.clear()
-    return "Your informations succesfully removed from on this page!"
+    return "<h1>Informations deleted succesfully!</h1>"
+
+
