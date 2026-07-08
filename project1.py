d68b6c0d3401c2a783e78428dec8ec8d5b6259ad
Mohammad Sina <mohammadsinazarei1102@gmail.com>
2026-07-09 00:37:04 +0330

    This time,I focused on the 'project1' file and I tested about myself...

--- project1.py
+++ project1.py
@@ -1,42 +1,85 @@
-from flask import Flask,request,render_template,session,redirect
+# Importing models
+from flask import Flask,request,redirect,render_template, session,url_for
+import psycopg
 from datetime import timedelta
+from werkzeug.security import generate_password_hash,check_password_hash
 
-
+# Doing initial settings
 app = Flask(__name__)
-app.secret_key = "Sina1234"
-app.permanent_session_lifetime = timedelta(minutes=1)
+app.secret_key = "Sinasecret123"
+app.permanent_session_lifetime = timedelta(minutes=2)
 
+# Connecting to postgres
+def connection():
+    return psycopg.connect(
+        host="localhost",
+        dbname="postgres",
+        user="postgres",
+        password="21200",
+        port=5432
+    )
+
+# Creating the table
+connect = connection()
+cur1 = connect.cursor()
+
+cur1.execute(
+    """CREATE TABLE IF NOT EXISTS siteusers(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(300) NOT NULL,
+    userid INTEGER NOT NULL
+
+    )"""
+)
+connect.commit()
+cur1.close()
+connect.close()
+
+# Main route
 @app.route("/")
-def index():
-    return render_template("form.html")
+def red_to_login():
+    return redirect(url_for("login"))
 
-@app.route("/login",methods=["POST"])
+@app.route("/login")
 def login():
     return render_template("loginform.html")
 
-@app.route("/result", methods=["GET", "POST"])
-def show_the_final_result():
+@app.route("/register" , methods=["POST"])
+def register():
+    return render_template("registerform.html")
 
-    if request.method == "POST":
+@app.route("/savinginfs",methods=["POST"])
+def saving_informations():
+    try:
         username = request.form.get("username")
+        password = request.form.get("password")
         userid = request.form.get("userid")
+
+        session["username"] = username
+        session["password"] = password
+        session["userid"] = userid
 
         session.permanent = True
 
-        session["username"] = username
-        session["userid"] = userid
+        secpassword = generate_password_hash(password)
 
-    if "username" not in session or session["username"] is None:
-        return redirect("/")
+        conn = connection()
+        postcur = conn.cursor()
 
-    return render_template(
-        "result.html",
-        username=session.get("username"),
-        userid=session.get("userid")
-    )
+        postcur.execute(
+            "INSERT INTO siteusers(username,password,userid) VALUES(%s,%s,%s)",
+            (username,secpassword,userid,)
+        )
+
+        conn.commit()
+        postcur.close()
+        conn.close()
+
+        return "<h3>Account created successfully!</h3>"
+    
+    except Exception as ex:
+        return f"Ops!!!!!!! : {ex}"
+    
 
 
-@app.route("/logout",methods=["POST"])
-def logout():
-    session.clear()
-    return redirect("/")
